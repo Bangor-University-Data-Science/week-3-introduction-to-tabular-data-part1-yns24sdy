c601cc823cf9fa0890e2ef80a0d354330d445a3e
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 16:08:04 +0100

    Update feature_type_dict.py

--- titanic_analysis/feature_type_dict.py
+++ titanic_analysis/feature_type_dict.py
@@ -1,13 +1,10 @@
 import pandas as pd
 
 def create_feature_type_dict(df):
-    feature_type_dict = {}
+    feature_types = {'numerical': [], 'categorical': []}
     for column in df.columns:
         if pd.api.types.is_numeric_dtype(df[column]):
-            if df[column].nunique() < 50:
-                feature_type_dict[column] = 'discrete numerical'
-            else:
-                feature_type_dict[column] = 'continuous numerical'
+            feature_types['numerical'].append(column)
         else:
-            feature_type_dict[column] = 'categorical'
-    return feature_type_dict
+            feature_types['categorical'].append(column)
+    return feature_types
