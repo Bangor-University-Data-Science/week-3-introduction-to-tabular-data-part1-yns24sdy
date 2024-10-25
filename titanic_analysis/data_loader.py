0f6d22106ad6f8d701cde722cd3bfd0f604eff1a
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 21:52:03 +0100

    Update data_loader.py

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -1,9 +1,9 @@
-import unittest
 import pandas as pd
 from titanic_analysis.data_loader import load_titanic_data
 
-
 def test_load_titanic_data():
-    df = load_titanic_data("/mnt/titanic.csv")
+    df = load_titanic_data("../../data/titanic.csv")
     assert isinstance(df, pd.DataFrame), "The returned object should be a DataFrame"
     assert not df.empty, "The DataFrame should not be empty"
+
+test_load_titanic_data()
