dcb2bc15fef6bfb4c61f19bc55d3cfac112ad48e
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 15:00:41 +0100

    Update feature_type_dict.py

--- titanic_analysis/feature_type_dict.py
+++ titanic_analysis/feature_type_dict.py
@@ -1,34 +1,9 @@
-import pandas as pd
-
-def create_feature_type_dict(df):
-    """
-    Classifies features into numerical (continuous or discrete) and categorical (nominal or ordinal).
-
-    Args:
-        df (pd.DataFrame): The Titanic dataset as a DataFrame.
-
-    Returns:
-        dict: A dictionary classifying features into numerical and categorical types.
-    """
-    feature_types = {
-        'numerical': {
-            'continuous': [], 
-            'discrete': []  
-        },
-        'categorical': {
-            'nominal': [],  
-            'ordinal': []  
-        }
-    }
+def create_feature_type_dict(df: pd.DataFrame) -> dict:
+    """Classifies features into numerical and categorical types."""
+    feature_types = {}
     for column in df.columns:
         if pd.api.types.is_numeric_dtype(df[column]):
-            if df[column].nunique() > 50:
-                feature_types['numerical']['continuous'].append(column)
-            else:
-                feature_types['numerical']['discrete'].append(column)
+            feature_types[column] = 'numerical'
         else:
-            if df[column].nunique() < 10:
-                feature_types['categorical']['ordinal'].append(column)
-            else:
-                feature_types['categorical']['nominal'].append(column)
+            feature_types[column] = 'categorical'
     return feature_types
