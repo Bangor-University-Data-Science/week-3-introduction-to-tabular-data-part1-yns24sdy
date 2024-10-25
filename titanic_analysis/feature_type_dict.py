843e54ca019e20f3edfed60707c887ea7e2e8f3a
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 18:38:23 +0100

    Update feature_type_dict.py

--- titanic_analysis/feature_type_dict.py
+++ titanic_analysis/feature_type_dict.py
@@ -1,32 +1,25 @@
+# Create module
 import pandas as pd
 
-# Load data
-df = pd.read_csv('/mnt/titanic.csv')
+def create_feature_type_dict(df):
+    # Define an empty dictionary to store feature types
+    feature_types = {'numerical': [], 'categorical': []}
 
-print('Basic information of data:')
-df.info()
-print('The first few rows of data:')
-print(df.head())
-
-# Define an empty dictionary to store feature types
-feature_types = {}
-
-# Traverse each column
-for col in df.columns:
-    # Determine whether the data type is numeric
-    if df[col].dtype in ['int64', 'float64']:
-        # Determine whether it is continuous (if the number of unique values is large, it is considered continuous)
-        if df[col].nunique() > 10:
-            feature_types[col] = 'numerical_continuous'
+    # Traverse each column
+    for col in df.columns:
+        # Determine whether the data type is numeric
+        if df[col].dtype in ['int64', 'float64']:
+            # Determine whether it is continuous (if the number of unique values is large, it is considered continuous)
+            if df[col].nunique() > 10:
+                feature_types['numerical'].append(col)
+            else:
+                feature_types['numerical'].append(col)
         else:
-            feature_types[col] = 'numerical_discrete'
-    else:
-        # Determine whether it is nominal (if the number of unique values is large, it is considered nominal)
-        if df[col].nunique() > 10:
-            feature_types[col] = 'categorical_nominal'
-        else:
-            feature_types[col] = 'categorical_ordinal'
+            # Determine whether it is nominal (if the number of unique values is large, it is considered nominal)
+            if df[col].nunique() > 10:
+                feature_types['categorical'].append(col)
+            else:
+                feature_types['categorical'].append(col)
 
-# Show feature type dictionary
-print('Feature type dictionary:')
-print(feature_types)
+    # Show feature type dictionary
+    return feature_types
