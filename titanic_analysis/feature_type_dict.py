00dd89ace39604b6f677a215009a0136e7808f84
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 15:26:05 +0100

    Update feature_type_dict.py

--- titanic_analysis/feature_type_dict.py
+++ titanic_analysis/feature_type_dict.py
@@ -1,9 +1,17 @@
+import pandas as pd
+
 def create_feature_type_dict(df: pd.DataFrame) -> dict:
     """Classifies features into numerical and categorical types."""
     feature_types = {}
+    numerical_columns = []
+    categorical_columns = []
     for column in df.columns:
         if pd.api.types.is_numeric_dtype(df[column]):
-            feature_types[column] = 'numerical'
+            numerical_columns.append(column)
         else:
-            feature_types[column] = 'categorical'
+            categorical_columns.append(column)
+    if numerical_columns:
+        feature_types['numerical'] = numerical_columns
+    if categorical_columns:
+        feature_types['categorical'] = categorical_columns
     return feature_types
