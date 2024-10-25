dce3378ba11fe9e85ef9286ab27e3d36a5d2a58e
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 15:45:47 +0100

    Update test_feature_type_dict.py

--- tests/test_feature_type_dict.py
+++ tests/test_feature_type_dict.py
@@ -1,13 +1,10 @@
-from titanic_analysis.feature_type_dict import create_feature_type_dict
 import pandas as pd
-def test_create_feature_type_dict():
-    # Mock a DataFrame
-    mock_df = pd.DataFrame(data={
-        'Age': [22, 38, 26, 35],
-        'Sex': ['male', 'female', 'female', 'male'],
-        'Survived': [0, 1, 1, 0]
-    })
-    feature_types = create_feature_type_dict(mock_df)
-    
-    assert 'numerical' in feature_types, "The dictionary should have a 'numerical' key"
-    assert 'categorical' in feature_types, "The dictionary should have a 'categorical' key"
+
+def create_feature_type_dict(df):
+    feature_type_dict = {}
+    for column in df.columns:
+        if pd.api.types.is_numeric_dtype(df[column]):
+            feature_type_dict[column] = 'numerical'
+        else:
+            feature_type_dict[column] = 'categorical'
+    return feature_type_dict
