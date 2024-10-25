cd680531752fcdc4f5416e0302764e68440d0962
yns24sdy <yns24sdy@bangor.ac.uk>
2024-10-25 18:34:33 +0100

    Update feature_type_dict.py

--- titanic_analysis/feature_type_dict.py
+++ titanic_analysis/feature_type_dict.py
@@ -1,12 +1,32 @@
-from titanic_analysis.feature_type_dict import create_feature_type_dict
 import pandas as pd
 
-def test_create_feature_type_dict():
-    mock_df = pd.DataFrame({
-        'Age': [22, 38, 26, 35],
-        'Sex': ['male', 'female', 'female', 'male'],
-        'Survived': [0, 1, 1, 0]
-    })
-    feature_types = create_feature_type_dict(mock_df)
-    expected_types = {'numerical_continuous', 'categorical_ordinal'}
-    assert set(feature_types.values()) & expected_types == expected_types
+# Load data
+df = pd.read_csv('/mnt/titanic.csv')
+
+print('Basic information of data:')
+df.info()
+print('The first few rows of data:')
+print(df.head())
+
+# Define an empty dictionary to store feature types
+feature_types = {}
+
+# Traverse each column
+for col in df.columns:
+    # Determine whether the data type is numeric
+    if df[col].dtype in ['int64', 'float64']:
+        # Determine whether it is continuous (if the number of unique values is large, it is considered continuous)
+        if df[col].nunique() > 10:
+            feature_types[col] = 'numerical_continuous'
+        else:
+            feature_types[col] = 'numerical_discrete'
+    else:
+        # Determine whether it is nominal (if the number of unique values is large, it is considered nominal)
+        if df[col].nunique() > 10:
+            feature_types[col] = 'categorical_nominal'
+        else:
+            feature_types[col] = 'categorical_ordinal'
+
+# Show feature type dictionary
+print('Feature type dictionary:')
+print(feature_types)
